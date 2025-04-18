d8d1590411876c4a45596f17b3c3b0d7e9605af3
Adam Torres Encarnacion <admr0805@gmail.com>
2025-04-18 00:38:02 -0400

    fix id problem

--- dataset_creation/geocell/cell.py
+++ dataset_creation/geocell/cell.py
@@ -405,6 +405,9 @@ class Cell:
             cell_collection.add(cell)
 
         # Remove dead cells from CellCollection
+        valid_ids = {c.admin_2 for c in cell_collection}
+        remove_cells = [cell for cell in remove_cells if cell.admin_2 in valid_ids]
+
         for cell in remove_cells:
             cell_collection.remove(cell)
 
